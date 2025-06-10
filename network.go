e7d22382cc3e466999a4b86ae342291b6938aa1a
Aryan Verma <58562093+AryanVerma2204@users.noreply.github.com>
2025-06-10 18:23:56 +0530

    Update network.go

--- network.go
+++ network.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"log"
 	"math/rand"
 	"sync"
 	"time"
@@ -18,7 +17,7 @@ type SimulatedNetwork struct {
 func NewSimulatedNetwork(config ExperimentConfig) *SimulatedNetwork {
 	channels := make([]chan Message, config.NumNodes)
 	for i := range channels {
-		channels[i] = make(chan Message, 1000) // Buffered channel
+		channels[i] = make(chan Message, 1000)
 	}
 	return &SimulatedNetwork{
 		config:   config,
@@ -32,19 +31,11 @@ func (s *SimulatedNetwork) GetChannel(nodeID int) chan Message {
 }
 
 func (s *SimulatedNetwork) Send(msg Message) {
-	// Simulate network conditions for a specific destination
 	s.wg.Add(1)
 	go func() {
 		defer s.wg.Done()
-
-		latency := s.config.NetworkLatency
-		// Check if the destination is a straggler
-		if stragglerCfg, ok := s.config.StragglerNodes[msg.To]; ok {
-			latency += stragglerCfg.ExtraLatency
-		}
-
 		select {
-		case <-time.After(latency):
+		case <-time.After(s.config.NetworkLatency):
 			if rand.Float64() > s.config.PacketLossProb {
 				s.channels[msg.To] <- msg
 			}
@@ -56,15 +47,8 @@ func (s *SimulatedNetwork) Send(msg Message) {
 
 func (s *SimulatedNetwork) Broadcast(fromNodeID int, payload interface{}) {
 	for i := 0; i < s.config.NumNodes; i++ {
-		if i == fromNodeID {
-			continue
-		}
+		if i == fromNodeID { continue }
 		msg := Message{From: fromNodeID, To: i, Payload: payload}
 		s.Send(msg)
 	}
-}
-
-func (s *SimulatedNetwork) Stop() {
-	close(s.stop)
-	s.wg.Wait()
 }
